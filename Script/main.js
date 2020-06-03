bc4f1d291536a8b416d43aa98762e72753ece0aa
Henrychen666 <henryc00183001@gmail.com>
2020-06-02 23:12:23 -0400

    Fix Drop down menu bugs

--- Script/main.js
+++ Script/main.js
@@ -18,6 +18,26 @@ function openInfo(evt, tabName) {
 
 }
 
+function openInfoDropdown(evt, tabName) {
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("dropdownbutn");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+
 
 	
 // generate a checkbox list from a list of products
